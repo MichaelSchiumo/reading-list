d8ee553f98fdee5fdf3a5891730c852d0b2a4594
vickicello <vicki.cello@gmail.com>
2018-09-19 14:49:24 -0400

    worked on user book controller, added user book views

---
+++ app/controllers/user_books_controller.rb
@@ -0,0 +1,14 @@
+class UserBooksController < ApplicationController
+  
+  get '/user_books/new' do
+     if logged_in?
+       @topics = Topic.all
+       @books = Book.all.select{|book| !(current_user.books.include?(book))}
+      erb :'user_books/new'
+     else
+      erb :'users/login'
+     end
+   end
+end
+
+#user can add comments about books
--- config.ru
+++ config.ru
@@ -5,7 +5,8 @@ if ActiveRecord::Migrator.needs_migration?
 end
 
 use Rack::MethodOverride
+use UserBooksController
+use UsersController
 use BooksController
 use TopicsController
-use UsersController
 run ApplicationController

20a02be691c4863c7006bec9ccc0f735b769693a
vickicello <vicki.cello@gmail.com>
2018-09-25 17:45:44 -0400

    Finished overhauling book controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,56 +14,85 @@ class BooksController < ApplicationController
     erb :'books/new'
     else
       flash[:message] = "You must be logged in to add a new book to your ReadingList."
-      redirect '/login'
+      redirect to '/login'
+    end
+  end
+
+  post '/books' do
+    if logged_in?
+      if params["book"]["title"] == "" || params["book"]["author"] == ""
+        flash[:message] = "Please fill in all required fields."
+        redirect to '/books/new'
+      else
+        @book = current_user.books.build(title: params[:title], author: params[:author])
+        if @book.save
+          redirect to '/books/#{@book.id}'
+        else
+          redirect to '/books/new'
+        end
+      end
+    else
+      redirect to '/login'
     end
   end
 
   get '/books/:id' do
     if logged_in?
-      @user = User.find_by(id: session[:user_id])
-      @book = Book.find_by(id: params["id"])
+      @book = Book.find_by(id: params[:id])
       erb :'/books/show'
     else
       flash[:message] = "You must be logged in to view this book."
-      redirect '/login'
+      redirect to '/login'
     end
   end
 
   get '/books/:id/edit' do
     if logged_in?
-      @book = Book.find_by(id: params["id"])
-      erb :'/books/edit'
+      @book = Book.find_by(id: params[:id])
+      if @book && @book.user == current_user
+        erb :'/books/edit'
+      else
+        redirect to '/books'
+      end
     else
       flash[:message] = "You must be logged in to edit your ReadingList."
-      redirect '/login'
-    end
-  end
-
-  post '/books' do
-    if params[:title] == "" || params[:author_name] == ""
-      flash[:message] = "Please fill in all required fields."
-      redirect '/books/new'
-    else
-      @user = User.find_by(id: session[:user_id])
-      @book = Book.find_or_create_by(title: params["book"]["title"], author: params["book"]["author"])
-      @book.save
-      redirect "/books/#{@book.id}"
+      redirect to '/login'
     end
   end
 
   patch '/books/:id' do
-    @user = User.find_by(id: session[:user_id])
-    @book = Book.find_by(id: params["id"])
-    @book.update(title: params["title"], author: params["author"])
-    flash[:message] = "Book sucessfully updated."
-    redirect "/users/#{@user.id}"
+    if logged_in?
+      if params["book"]["title"] == "" || params["book"]["author"] == ""
+        redirect to '/books/#{@book.id}/edit'
+        # redirect to "/books/#{params[:id]}/edit" ?????
+      else
+        @book = Book.find_by(id: params[:id])
+        if @book && @book.user == current_user
+          if @book.update(title: params["title"], author: params["author"])
+            flash[:message] = "Book sucessfully updated."
+            redirect to '/books/#{@book.id}'
+          else
+            redirect to '/books/#{@book.id}/edit'
+          end
+        else
+          redirect to '/books'
+        end
+      end
+    else
+      redirect to '/login'
+    end
   end
 
   delete '/books/:id/delete' do
-    @user = User.find_by(id: session[:user_id])
-    @book = Book.find_by(id: params["id"])
-    @book.destroy
-    flash[:message] = "Your book has been deleted."
-    redirect "/users/#{@user.id}"
+    if logged_in?
+      @book = Book.find_by(id: params["id"])
+      if @book && @book.user == current_user
+        @book.delete
+      end
+      flash[:message] = "Your book has been deleted."
+      redirect to '/books'
+    else
+      redirect to '/login'
+    end
   end
 end

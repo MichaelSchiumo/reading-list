cfd13b9117a7975afeb2c152d6558233357b9493
vickicello <vicki.cello@gmail.com>
2018-09-25 15:09:53 -0400

    reset db and migrations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,13 +1,3 @@
 class Book < ActiveRecord::Base
   belongs_to :user
-
-#   def slug
-#     self.title.split(/\W/).join("-").downcase
-#   end
-
-#   def self.find_by_slug(slug)
-#     self.all.find do |book|
-#       book.slug == slug
-#     end
-#   end
 end
